2023514742fe50684defb762faf3ae7364180139
Nicholas Finch <ngf4@nau.edu>
2022-11-13 10:41:58 -0700

    Update index.js

--- Week-05-Object_Oriented_Programming/Wee-05-AssignmentMenuApp/index.js
+++ Week-05-Object_Oriented_Programming/Wee-05-AssignmentMenuApp/index.js
@@ -1,30 +1,122 @@
-class Card {
-    constructor(name, position) {
 
+
+/*
+We will be creating a simple todo app
+Starting from the top down, we have a TodoBoard, which can have many TodoItems
+each TodoItem will have tickets, along with a title and a description.
+
+A user may instantiate a new TodoBoard like below
+let NicholasBoard = new TodoBoard("Nicholas's Board")
+NicholasBoard.addNewItem("complete app", "You have an app that you need to complete!")
+NicholasBoard.addNewItem("submit app idea", "Go to promineotech and submit your completed app")
+NicholasBoard.displayTodoItems()
+
+Now to set it up as an actual usable selection, we'll setup a separate a Main() function.
+
+* Might make sense to have everything in an object
+*/
+let exampleTodoBoards = []
+
+exampleTodoBoards.push({
+    name: "John",
+    items: [
+        {
+            title: "pushups",
+            description: "complete 10 pushups",
+        },
+        {
+            title: "pullups",
+            description: "complete at least 5 pullups"
+        }
+    ],
+})
+
+exampleTodoBoards.push(
+    {
+        name: "Nicholas",
+        items: [
+            {
+                title: "complete app",
+                description: "You have an app that you need to complete!"
+            },
+            {
+                title: "submit app",
+                description: "turn in app before the 10th"
+            }
+        ]
     }
+)
+/*
+=================== How the app may potentially look ===================
+Welcome to the JS Todo App
+No boards created
+Create a new board
 
-    describe() {
 
+Welcome to the JS Todo App
+Select a board
+0) John's Board
+1) Nicholas's Board
+e) Exit
+Make a selection
+Selection: 1
+
+You selected Nicholas's Board
+Select a Todo item to view
+0) complete app
+1) submit app idea
+e) Exit
+Selection: 1
+
+0 - complete app:
+You have an app that you need to complete!
+0) Mark as complete
+e) Exit
+Selection: 0
+
+You selected Nicholas's Board
+Select a Todo item to view
+0) submit app idea
+e) Exit
+Selection: e
+
+Welcome to the JS Todo App
+Select a board
+0) John's Board
+1) Nicholas's Board
+e) Exit
+Make a selection
+Selection: e
+
+>Goodbye
+
+*/
+
+class Item {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
     }
 }
 
-class Vertical {
+class Owner {
     constructor(name) {
-
+        this.name = name
+        this.items = []
     }
 
-    addPlayer(player) {
-
+    addItem(title, description) {
+        this.items.push(new Item(title, description))
     }
 
-    describe() {
-
+    delItem(index) {
+        this.items.splice(index, 1)
     }
 }
 
 class Menu {
     constructor() {
-
+        this.owners = []
     }
 
     start() {
@@ -32,16 +124,16 @@ class Menu {
         while (selection != 0) {
             switch (selection) {
                 case "1":
-                    this.createTeam();
+                    this.createOwner();
                     break;
                 case "2":
-                    this.viewTeam();
+                    this.viewOwner();
                     break;
                 case "3":
-                    this.deleteTeam();
+                    this.deleteOwner();
                     break;
                 case "4":
-                    this.displayAllTeams();
+                    this.displayOwners();
                     break;
                 default:
                     selection = 0;
@@ -51,40 +143,46 @@ class Menu {
         alert("Good bye");
     }
 
+    showTitle() {
+        return 'Welcome to the JS Todo App';
+    }
+
     showMainMenuOptions() {
         return prompt(`
-          0) Exit
-          1) Create a new Team
-          2) View a Team
-          3) Delete a Team
-          4) Display all Teams
+            Welcome to the JS Todo App
+            Select a board
+            0) Exit
+            1) Create a new Team
+            2) View a Team
+            3) Delete a Team
+            4) Display all Teams
           `);
     }
 
-    showTeamMenuOptions(teamInfo) {
+    showItemMenuOptions(teamInfo) {
         return prompt(`
-          0) Back
-          1) Create a new Player
-          2) Delete a Player
+            0) Back
+            1) Create a new Item
+            2) Delete a Player
           ------------------------
           ${teamInfo}
           `);
     }
 
-    createTeam() {
+    createOwner() {
         let name = prompt("Enter name for new Team:");
         let team = new Team(name);
         this.teams.push(team);
     }
 
-    deleteTeam() {
+    deleteOwner() {
         let index = prompt("Enter index of Team to delete");
         if (index > -1 && index < this.teams.length) {
             this.teams.splice(index, 1);
         }
     }
 
-    displayAllTeams() {
+    displayOwners() {
         let teamString = "";
         for (let i = 0; i < this.teams.length; i++) {
             teamString += i + ") " + this.teams[i].name + "\n";
@@ -92,7 +190,7 @@ class Menu {
         alert(teamString);
     }
 
-    viewTeam() {
+    viewOwner() {
         let index = prompt("Enter index of Team to view:");
         if (index > -1 && index < this.teams.length) {
             this.selectedTeam = this.teams[index];
