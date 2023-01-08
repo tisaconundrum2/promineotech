ffd0d71a0e425a4823bd7b5f1fd06bb83e5bc2f4
Nicholas Finch <ngf4@nau.edu>
2023-01-08 15:41:15 -0700

    GET and POST

--- Week-11-JQuery_and_AJAX/InstallingAndUsingJquery/app.js
+++ Week-11-JQuery_and_AJAX/InstallingAndUsingJquery/app.js
@@ -1,22 +1,6 @@
-let p = $('#test');
-let div = $('.my-class')
-let ul = $('ul')
+$.get('https://reqres.in/api/users/2', (data) => console.log(data));
 
-console.log(p);
-console.log(div);
-console.log(ul);
-console.log(p.text());
-p.text('New Text')
-
-$('input').attr('placeholder', 'Placeholder text')
-
-div.prepend('<p>Prepended paragraph</p>');;
-div.append('<p>Appended paragraph</p>');
-div.before('<p>Before the div');
-div.after('<p>After the div');
-
-div.empty();
-ul.remove();
-
-$('input').hide();
-setTimeout(() => $('input').show(), 2000)
+$.post('https://reqres.in/api/users', {
+    name: 'Tommy',
+    job: 'FrontEnd Software Developer'
+}, (data) => console.log(data));
