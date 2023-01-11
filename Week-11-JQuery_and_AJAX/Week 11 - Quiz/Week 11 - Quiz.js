73223134797d50b2f718c9ef4fbc746a471fd9de
Nicholas Finch <ngf4@nau.edu>
2023-01-11 05:46:39 -0700

    adding week 11

---
+++ Week-11-JQuery_and_AJAX/Week 11 - Quiz/Week 11 - Quiz.js
@@ -0,0 +1,22 @@
+/**
+ * Assume that you have an array of customer objects like this:
+
+[
+  {
+    firstName: "Sam",
+    lastName: "Smith",
+    email: "sam.smith@gmail.com"
+  },
+  {
+    firstName: "Tom",
+    lastName: "Jones",
+    email: "tom.jones@gmail.com"
+  },
+......
+]
+Also, assume your HTML contains a table with an id of "customer-table" 
+columns First Name, Last Name, and Email Address. 
+Using JQuery, write the code needed to add a new row to the table for each customer in the returned array.
+
+ */
+
